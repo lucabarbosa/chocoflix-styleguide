d3ad6f339856d1da66fc3e9ab77571dd44961b5b
Lucas Barbosa <barbosa.lucash@gmail.com>
2019-09-15 18:11:24 -0300

    feat: add beta colors theme

---
+++ src/theme/index.js
@@ -0,0 +1,18 @@
+export default {
+  background: '#FFF',
+  surface: '#F9F9F9',
+  onBackground: '#303030',
+  onSurface: '#303030',
+  primary: ['#F00', '#F44E3F', '#F97E6D'],
+  danger: ['#D32F2F', '#F44336', '#F8877F'],
+  alert: ['#FFA000', '#FFC107', '#FFD761'],
+  success: ['#28a745', '#4CAF50', '#7CC47F'],
+  light: ['#f8f9fa', '#e2e6ea', '#f8f9fa'],
+  dark: ['#343a40', '#23272b', '#303030'],
+  onPrimary: '#FFF',
+  onDanger: '#FFF',
+  onAlert: '#303030',
+  onSuccess: '#FFF',
+  onLight: '#303030',
+  onDark: '#FFF',
+}
